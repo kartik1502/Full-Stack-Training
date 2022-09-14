b1a775d46f76f39e50cffd449a3a2f6c1e5cd703
kartik1502 <kartikkulkarni1411@gmail.com>
2022-09-14 15:38:01 +0530

    Hibernate Core

---
+++ Hibernate Core/hibernatedemo/src/main/java/org/hibernatedemo/employee/dao/EmployeeDao.java
@@ -0,0 +1,93 @@
+package org.hibernatedemo.employee.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernatedemo.employee.dto.Employee;
+
+public class EmployeeDao {
+
+	StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.config.xml").build();
+	Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+	SessionFactory factory = metadata.getSessionFactoryBuilder().build();
+
+	Transaction transaction = null;
+
+	public void saveEmployee(Employee employee) {
+
+		Session session = factory.openSession();
+		try {
+			transaction = session.beginTransaction();
+			session.save(employee);
+			transaction.commit();
+		} catch (HibernateException he) {
+			if (transaction != null)
+				transaction.rollback();
+			he.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	public List<Employee> getEmployeeDetails() {
+
+		List<Employee> employees = new ArrayList<>();
+		Session session = factory.openSession();
+		try {
+			transaction = session.beginTransaction();
+			employees = session.createQuery("From Employee").list();
+			transaction.commit();
+		} catch (HibernateException he) {
+			if (transaction != null)
+				transaction.rollback();
+			he.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return employees;
+
+	}
+
+	public void updateEmployee(int id, String empName, String empEmail) {
+
+		Session session = factory.openSession();
+		try {
+			transaction = session.beginTransaction();
+			Employee employee = session.get(Employee.class, id);
+			employee.setEmpName(empName);
+			employee.setEmpEmail(empEmail);
+			session.update(employee);
+			transaction.commit();
+		} catch (HibernateException he) {
+			if (transaction != null)
+				transaction.rollback();
+			he.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	public void deleteEmployeeById(int id) {
+		Session session = factory.openSession();
+		try {
+			transaction = session.beginTransaction();
+			Employee employee = session.get(Employee.class, id);
+			session.delete(employee);
+			transaction.commit();
+		} catch (HibernateException he) {
+			if (transaction != null)
+				transaction.rollback();
+			he.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+}
---
+++ Hibernate Core/hibernatedemo/src/main/java/org/hibernatedemo/employee/dto/Employee.java
@@ -0,0 +1,41 @@
+package org.hibernatedemo.employee.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int empId;
+	private String empName;
+	private String empEmail;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpEmail() {
+		return empEmail;
+	}
+
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+
+}
