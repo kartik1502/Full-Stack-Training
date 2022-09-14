32545a7e9c644ec1a1dff49fddecf119914484c4
kartik1502 <kartikkulkarni1411@gmail.com>
2022-09-14 18:05:44 +0530

    hibernate

---
+++ Hibernate Core/Using XML for Mapping/XML-hibernatedemo/src/main/java/org/xml_hibernatedemo/employee/controller/EmployeeController.java
@@ -0,0 +1,64 @@
+package org.xml_hibernatedemo.employee.controller;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import org.xml_hibernatedemo.employee.dao.EmployeeDao;
+import org.xml_hibernatedemo.employee.dto.Employee;
+
+public class EmployeeController {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		EmployeeDao employeeDao = new EmployeeDao();
+		boolean execute = true;
+		while(execute){
+			System.out.println("1.Save the employee.\n2.Get the Employee Details.\n3.Update Employee By Id.\n4.Delete Employee by Id.\n5.Terminate.\nEnter your choice");
+			int choice = sc.nextInt();
+			switch(choice){
+				case 1:{
+					Employee employee = new Employee();
+					System.out.println("Enter the Employee Name: ");
+					employee.setEmpName(sc.next());
+					System.out.println("Enter the Employee Email Id: ");
+					employee.setEmpEmail(sc.next());
+					employeeDao.saveEmployee(employee);
+				}
+					break;
+				case 2:{
+					List<Employee> employees = employeeDao.getEmployeeDetails();
+					Iterator< Employee> it = employees.iterator();
+					while(it.hasNext()){
+						Employee employee = it.next();
+						System.out.println("Employee Id: "+employee.getEmpId());
+						System.out.println("Employee Name: "+employee.getEmpName());
+						System.out.println("Employee Email Id: "+employee.getEmpEmail());
+					}
+				}
+					break;
+				case 3:{
+					System.out.println("Enter the Employee id: ");
+					int id = sc.nextInt();
+					System.out.println("Enter the Employee Name: ");
+					String name = sc.next();
+					System.out.println("Enter the Employee Email Id: ");
+					String emailId = sc.next();
+					employeeDao.updateEmployee(id, name, emailId);
+				}
+					break;
+				case 4:{
+					System.out.println("Enter the Employee Id: ");
+					int id = sc.nextInt();
+					employeeDao.deleteEmployeeById(id);
+				}
+					break;
+				case 5: execute = false;
+					break;
+				default:System.out.println("Invalid choice\n");
+			}
+		}
+		sc.close();
+	}
+}
---
+++ Hibernate Core/Using XML for Mapping/XML-hibernatedemo/src/main/java/org/xml_hibernatedemo/employee/dto/Employee.java
@@ -0,0 +1,33 @@
+package org.xml_hibernatedemo.employee.dto;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String empEmail;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpEmail() {
+		return empEmail;
+	}
+
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+
+}
